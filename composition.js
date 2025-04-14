49f47a50c7642993fc4e190d8066da8bc7aa5f45
umbereye27 <carineumbereye7@gmail.com>
2025-04-14 06:21:29 +0200

    Implement unique array exercise

--- composition.js
+++ composition.js
@@ -18,29 +18,3 @@ const shoutBackwards = compose(capitalize, reverse);
 
 console.log(shoutBackwards('hello world'));
 
-// 2. Write a function uniqueElements(arr1, arr2) that takes two arrays and returns a new array with unique elements from both arrays, maintaining the original order
-
-function uniqueElements(arr1, arr2) {
-    const seen = new Set([...arr1, ...arr2]);
-    console.log(seen);
-    return [...seen];
-  }
-  console.log(uniqueElements([1, 2, 3, 3], [3, 4, 5]));
-  
-
-//   3.Create a function that performs a JOIN operation on two arrays of objects, based on a common property, and returns a new array of merged objects.
-
-const employees = [
-    { id: 1, name: 'John', department: 'IT' },
-    { id: 2, name: 'Jane', department: 'HR' },
-    { id: 3, name: 'Doe', department: 'IT' }
-    ];
-    const salaries = [
-    { id: 1, salary: 50000 },
-    { id: 2, salary: 60000 },
-    { id: 4, salary: 55000 }
-    ];
-    
-    const resul = sqlJoin(employees, salaries);
-    console.log(result);
-    
