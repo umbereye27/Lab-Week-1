f77f3a1214417781c6eeb0531bf9545964f4139c
umbereye27 <carineumbereye7@gmail.com>
2025-04-14 06:22:15 +0200

    Implement unique array exercise

--- uniqueElements.js
+++ uniqueElements.js
@@ -1,6 +1,6 @@
 // 2. Write a function uniqueElements(arr1, arr2) that takes two arrays and returns a new array with unique elements from both arrays, maintaining the original order 
-function uniqueElements(arr1, arr2) { 
-    const seen = new Set([...arr1, ...arr2]); 
-    console.log(seen); return [...seen]; } 
-    console.log(uniqueElements([1, 2, 3, 3], [3, 4, 5]));
-     
+function uniqueElements(arr1, arr2) {
+    const seen = new Set([...arr1, ...arr2]);
+    console.log(seen); return [...seen];
+}
+console.log(uniqueElements([1, 2, 3, 3], [3, 4, 5]));
